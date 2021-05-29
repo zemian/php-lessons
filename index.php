017ab6636011f40b12808d2635a260dbeb697f27
Zemian Deng <zemian.deng@oracle.com>
2021-05-29 13:51:02 -0400

    Add bulma.css and vuejs and pretty up index

--- index.php
+++ index.php
@@ -2,34 +2,65 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/vue.js"></script>
     <title>PHP Lessons</title>
 </head>
 <body>
-
-<h1>PHP Lessons</h1>
-<table>
-    <tr valign="top">
-        <td>
-            <pre><?php include 'readme.md' ?></pre>
-        </td>
-        <td>
-            <ul>
-                <?php
-                $files = scandir(".");
-                foreach ($files as $file) {
-                    // Skip all dot files
-                    if (substr_compare($file, '.', 0, 1) === 0)
-                        continue;
-                    echo "<li><a href='$file'>$file</a></li>";
-                }
-                ?>
-            </ul>
-        </td>
-    </tr>
-</table>
-
+<div id="app">
+    <nav class="navbar is-dark">
+        <div class="navbar-burger" data-target="navbar-menu"
+             :class="{'is-active': isMobileMenuActive}" @click="toggleIsMobileMenuActive">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <div class="navbar-menu" id="navbar-menu" :class="{'is-active': isMobileMenuActive}">
+            <div class="navbar-start">
+                <a class="navbar-item">Home</a>
+                <a class="navbar-item">Basic</a>
+                <a class="navbar-item">Common Tasks</a>
+            </div>
+        </div>
+    </nav>
+    <section class="hero is-large is-primary">
+        <div class="hero-body">
+            <div class="columns m-4">
+                <div class="column">
+                    <div class="notification is-danger notification-box">
+                        PHP
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="notification is-warning notification-box">
+                        HTML/CSS/JavaScript
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="notification is-info notification-box">
+                        Bulma/VueJS
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer class="footer has-text-centered">
+        Zemian Deng &copy; <?php echo date('Y'); ?>
+    </footer>
+</div>
+<script>
+    new Vue({
+        data: function () {
+            return {
+                isMobileMenuActive: null
+            }
+        },
+        methods: {
+            toggleIsMobileMenuActive() {
+                this.isMobileMenuActive = (this.isMobileMenuActive ? null : true);
+            }
+        }
+    }).$mount('#app');
+</script>
 </body>
 </html>
