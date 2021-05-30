64fade8b7389384825fe47f1eff2d2a8bf23260f
Zemian Deng <zemian.deng@oracle.com>
2021-05-30 19:19:26 -0400

    Add list examples

---
+++ basic/02b_html-text-list.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>PHP Lessons</title>
+</head>
+<body>
+    <h1>Header, Text and List</h1>
+    <h2>Sub header</h2>
+    <p>We <b>will</b> <i>learn</i> about list <code>below</code>.
+        Look a <button>button</button> is also an inline element.</p>
+    <ul>
+        <li>one</li>
+        <li>two</li>
+        <li>three</li>
+    </ul>
+    <ol>
+        <li>one</li>
+        <li>two</li>
+        <li>three</li>
+    </ol>
+    <p>Thank you for participating.</p>
+</body>
+</html>
---
+++ basic/03b_control-statements.php
@@ -0,0 +1,33 @@
+<?php
+
+// Operators: +, -, *, /, ()
+// Comparators: <, >, <=, >=, ===, ?:, ??
+
+// if, else, else if
+// for, while
+// switch
+
+$n = 10;
+if ($n < 10) {
+    echo "n is less than 10";
+}
+
+$x = null;
+if ($x) {
+    echo "x is true";
+} else {
+    echo "x is false";
+}
+
+if (isset($y)) {
+    echo "y is set";
+}
+
+for ($i = 0; $i < 10; $i++) {
+    echo $i;
+}
+
+$j = 0;
+while ($j < 10) {
+    echo $j;
+}
