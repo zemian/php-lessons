eb155d8eb7ecc63ed29e51d99aa5f16b9972d5eb
Zemian Deng <zemian.deng@oracle.com>
2021-06-01 08:07:12 -0400

    Renamed files

--- basic/06_more-on-array.php
+++
@@ -1,5 +0,0 @@
-<?php
-// array can be use as map, and it can be convert into object and back.
-// json_encode() will return different result for empty array() vs empty object.
-
-
--- basic/07_more-on-strings.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Heredoc vs nowdoc
-$text = <<< HERE
-A here doc is for
-multiline entries
-HERE;
-echo $text;
-
