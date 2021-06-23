2d5c204d2eaa0b4c49a36d490053747ec75c46f4
Zemian Deng <zemiandeng@gmail.com>
2021-06-23 09:56:50 -0400

    Add personal site assignment

---
+++ examples/personal-site.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Assignment: Build a personal website that contains following:
+ *
+ * A navigation header that always show on all pages. It should have links
+ * to following items:
+ *
+ *   - Home => it's the index.php that show case your "Landing" page.
+ *             This page can contains any thing, so be creative. Try to capture your
+ *             web visitor attention and make them want to brose the rest of your site.
+ *   - About => A page that contains one photo and at least one paragraph
+ *              text to describe who you are.
+ *   - Hobbies => A page with at least 3 sections.
+ *              Each section list one of your favorite hobby or accomplishments.
+ *              (use different background with at least 800px height each).
+ *   - GuestBook => A page to display a form that allow user to leave you
+ *              comments on the site. Minimum field is Comment message, and optionally
+ *              user may enter their Name and Email if they choose to enter it.
+ *              Below the form list all of saved comments and sorted by most recent datetime.
+ *              It should display their Name if given, but do not show email address.
+ *              Use file system and JSON format to store the comments text.
+ */
